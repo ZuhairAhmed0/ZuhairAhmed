eb967afbdd65c82be106460849bab3bda7739fab
ZuhairAhmed0 <zuh1290@gmail.com>
2022-05-18 12:51:58 +0200

    last update

--- js/getResponse.js
+++ js/getResponse.js
@@ -1,10 +1,10 @@
 async function getResponse(url) {
-  const response = await await fetch(`http://localhost:3000/api/${url}`, {
-    method: "GET",
-  });
-//   const response = await fetch(`https://zuhair-api.herokuapp.com/api/${url}`, {
+//   const response = await await fetch(`http://localhost:3000/api/${url}`, {
 //     method: "GET",
 //   });
+  const response = await fetch(`https://zuhair-api.herokuapp.com/api/${url}`, {
+    method: "GET",
+  });
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
